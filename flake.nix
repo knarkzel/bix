439b723680ce40c070c839e09ac0b75992843a68
Knarkzel <knarkzel@gmail.com>
2023-09-02 12:23:16 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, }: let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    buildBunPackage = { src, packages, hash, config ? {} }: let
+      packageJSON = pkgs.lib.importJSON packages;
+      site-src = pkgs.buildNpmPackage {
+        inherit src config;
+        pname = "${packageJSON.name}";
+        version = packageJSON.version;
+        npmDepsHash = hash;
+        installPhase = ''
+          cp -r . $out
+        '';
+      };
+    in {
+      packages.default = pkgs.writeShellScriptBin "${packageJSON.name}" ''
+        ${pkgs.nodejs}/bin/node ${site-src}/build
+      ''; 
+    };
+  };
+}
