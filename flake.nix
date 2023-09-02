8f447b4e9071c6ac18771778e6a78732f2cae1ac
Knarkzel <knarkzel@gmail.com>
2023-09-02 12:59:57 +0200

    merge attrset

--- flake.nix
+++ flake.nix
@@ -3,26 +3,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, }: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
   in {
-    buildBunPackage = { src, packages, hash, config ? {} }: let
+    buildBunPackage = {
+      src,
+      packages,
+      hash,
+      config ? {},
+    }: let
       packageJSON = pkgs.lib.importJSON packages;
       site-src = pkgs.buildNpmPackage {
-        inherit src config;
+        inherit src;
         pname = "${packageJSON.name}";
         version = packageJSON.version;
         npmDepsHash = hash;
         installPhase = ''
           cp -r . $out
         '';
-      };
+      } // config;
     in {
       pkgs.writeShellScriptBin = "${packageJSON.name}" ''
         ${pkgs.bun}/bin/bun ${site-src}/build
-      ''; 
+      '';
     };
   };
 }
